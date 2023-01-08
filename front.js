a6e1287b2b50ece00db59146cc09a7d2e6d4e1d2
Urmi Mahapatra <urmi.mahapatra@gmail.com>
2023-01-09 01:57:46 +0530

    CalculatorPrj

---
+++ front.js
@@ -0,0 +1,79 @@
+prev = 0;
+curr = 0;
+currOp = "";
+isFloat = false;
+
+function Clicked(input) {
+    let c1 = checkedNumOrOp(input);
+    if(isNaN(c1)) {
+        processOperator(input);
+    } else {
+        if(isFloat) {
+            curr = curr + c1*0.1;
+        } else {
+            curr = curr * 10 + c1;
+        }
+        displayText(curr);
+    }
+}
+
+
+function clearDisplay(){
+    document.getElementById("display").innerHTML="";
+    prev = 0;
+    curr = 0;
+    currOp = "";
+    isFloat = false;
+}
+
+function displayText(text) {
+    document.getElementById("display").innerHTML = text;
+}
+
+function checkedNumOrOp(inChar) {
+    return parseInt(inChar);
+}
+
+function processOperator(inChar) {
+    alert(inChar);
+    if(inChar == "+/-") {
+        curr = -curr;
+        displayText(curr);
+    } 
+    else if(inChar == ".") {
+        curr = curr * 1.0;
+        isFloat = true;
+        displayText(curr);
+    } 
+    else if(inChar == "=") {
+        solve(currOp);
+        currOp = "";
+    } 
+    else if(currOp == "") {
+        currOp = inChar;
+        prev = curr;
+        curr = 0;
+        displayText(prev);
+    }
+    else {
+        solve(currOp);
+        currOp = inChar;
+    }
+}
+
+function solve(operator){
+    if(operator == "") {
+    } else if(operator =="+") {
+        prev = prev + curr;
+    } else if(operator =="-") {
+        prev = prev - curr;
+    } else if(operator =="/") {
+        prev = prev / curr;
+    } else if(operator =="*") {
+        prev = prev * curr;
+    } else if(operator =="%") {
+        prev = prev * curr / 100;
+    }
+    curr = 0;
+    displayText(prev);
+}
